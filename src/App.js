1f218916a22cd4b5aea8714717887691d1dfdbf8
Vicky <vicky@arimarcssiPhone.localdomain>
2020-01-09 18:19:34 +0000

    cleaning up the app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to KodFlix
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>KodFlix</h1>
+    
     </div>
   );
 }
 
 export default App;
+
