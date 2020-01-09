d86234d9dc2ae7d6419d5b2848af8e695e2749dd
Vicky <vicky@arimarcssiPhone.localdomain>
2020-01-09 20:23:41 +0000

    add cover image

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
+import film1 from './molly2.jpg';
 
 function App() {
   return (
     <div className="App">
       <div className="container">
       <h1>KodFlix</h1>
+      <img src={film1}/>
+
       </div>
       <nav>Navbar</nav>
       <aside>Menu</aside>
