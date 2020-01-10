c265041cb73749c33a02932cdfabd3861231cc64
Vicky <vicky@Vickys-MacBook-Pro.local>
2020-01-10 17:13:17 +0000

    footer in progress

--- src/App.js
+++ src/App.js
@@ -4,17 +4,34 @@ import film1 from './molly.jpg';
 
 function App() {
   return (
-    <div className="App">
-      <div className="container">
-      <h1>KodFlix</h1>
-      <img src={film1}/>
-
-      </div>
-      <nav>Navbar</nav>
+  <div className="App">
+    <h1>KodFlix</h1>
+    <img src={film1}/>
+    <div className="container">
+      <nav className='nav'>Navbar</nav>
       <aside>Menu</aside>
       <article>Article</article>
-      <footer>Footer</footer>
-    
+
+
+
+      <footer id="footer">
+        <div className="row">
+          <ul className="footer-nav">
+            <li><a href="#">Menu</a></li>
+            <li>Adress: 199 Eade Road</li>
+            <li>Email: vasiliki.robles@gmail.com</li>
+            <li>Contact: +44 743 857 7974</li>
+          </ul>
+        </div>
+
+      <div className="copywrite">
+        <p>Copywriting 2020. KodFlix all rights reserved.</p>
+
+      </div>
+
+      </footer>
+    </div>
+
     </div>
   );
 
