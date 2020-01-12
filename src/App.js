69a526aa8391325c35015068b8c109ee54938b6e
Vicky <vicky@Vickys-MacBook-Pro.local>
2020-01-12 22:11:53 +0000

    first components

--- src/App.js
+++ src/App.js
@@ -4,72 +4,53 @@ import molly from './mollysgame.jpg';
 
 function App() {
   return (
-  <div className="App">
+    <div className="App">
 
-<body>
-<h1>KodFlix</h1>
-
-
-<nav>Nav bar</nav>
-
-<div className="container">
-  <aside>Menu</aside>
-  <article>Article</article>
-</div>
+      <body>
+        <h1>KodFlix</h1>
 
 
+        <nav>Nav bar</nav>
+        <div className="container">
+          <aside>Menu</aside>
+          <article>Article</article>
+        </div>
 
-<img src={molly} className="header-image" alt='molly logo'/>
+        <img src={molly} className="header-image" alt='molly logo' />
 
+        <div className="container">
+          <Movietitle name="Black Mirror" picture="box box1" />
+          <Movietitle name="Suits" picture="box box2" />
+          <Movietitle name="The Witcher" picture="box box3" />
+        </div>
 
-<div className="container">
-  <div className="box box1">
-    <div className="overlay">
-      <h2>Black Mirror</h2>
-      </div>
-    </div>
-<div className="box box2">
-    <div className="overlay">
-    <h2>Suits</h2>
-    </div>
-    </div>
-  <div className="box box3">
-    <div className="overlay">
-      <h2>Witcher</h2>
-      </div>
-    </div>
-  </div>
+        <div className="container">
+          <Movietitle name="Bleach" picture="" />
+          <Movietitle name="Lucifer" picture="" />
+          <Movietitle name="Fairytail" picture="" />
+        </div>
 
-<div className="container">
-  <div className="box box4">
-    <div className="overlay">
-      <h2>Bleach</h2>
-    </div>
+        <footer>Footer</footer>
+      </body>
     </div>
 
-  <div className="box box5">
-    <div className="overlay">
-      <h2>Lucifer</h2>
-      </div>
-    </div>
 
-  <div className="box box6">
-    <div className="overlay">
-    <h2>Fairytail</h2>
-    </div>
-    </div>
-</div>
-
-
-<footer>Footer</footer>  
-</body>  
-     
-</div>
- 
-   
   );
 
-  }
+}
+
+function Movietitle(props) {
+  return (
+    <div className={props.picture}>
+      <div className="overlay">
+        <h2>{props.name}</h2>
+      </div>
+    </div>
+  );
+}
+
+
+
 
 
 
