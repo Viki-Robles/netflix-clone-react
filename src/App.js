ff62546f8f01261fb1c959715f02c86f1c3136f3
Vicky <vicky@Vickys-MacBook-Pro.local>
2020-01-12 17:08:13 +0000

    h2 trying to position it in the center

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
 
 
 
-<img src={molly} alt='molly logo'/>
+<img src={molly} className="header-image" alt='molly logo'/>
 
 
 <div className="container">
@@ -27,15 +27,29 @@ function App() {
     <div className="overlay">
       <h2>Black Mirror</h2></div>
     </div>
-
-  <div className="box box2"><h2>Suits</h2></div>
-  <div className="box box3"><h2>Witcher</h2></div>
+<div className="box box2">
+    <div className="overlay">
+    <h2>Suits</h2></div>
+    </div>
+  <div className="box box3">
+    <div className="overlay">
+      <h2>Witcher</h2></div>
+    </div>
   </div>
 
 <div className="container">
-  <div className="box box4"><h2>Bleach</h2></div>
-  <div className="box box5"><h2>Lucifer</h2></div>
-  <div className="box box6"><h2>Fairytail</h2></div>
+  <div className="box box4">
+    <div className="overlay">
+      <h2>Bleach</h2>
+    </div></div>
+
+  <div className="box box5">
+    <div><h2>Lucifer</h2></div>
+    </div>
+
+  <div className="box box6">
+    <div className="overlay">
+    <h2>Fairytail</h2></div></div>
 </div>
 
 
