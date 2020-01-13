36733a60b58be4cd9d55b0a1cf10f0df6ba6a995
Vicky <vicky@arimarcssiPhone.localdomain>
2020-01-13 19:48:09 +0000

    components practise

--- src/App.js
+++ src/App.js
@@ -17,44 +17,68 @@ function App() {
 
       <img src={molly} className="header-image" alt='molly logo' />
 
-      <div className="container">
-
-      <MovieTitle name="Black Mirror" picture="box box1"/>
-      <MovieTitle name="Suits" picture="box box2"/>
-      <MovieTitle name="The Witcher" picture="box box3"/>
-
-      <div className="container">
-
-      <MovieTitle name="Bleach" picture="box box4"/>
-      <MovieTitle name="Lucifer" picture="box box5"/>
-      <MovieTitle name="Fairytail" picture="box box6"/>
-      </div>
-
-
       
 
-     
 
+     {/* <div className="container">
 
+        <MovieTitle name="Black Mirror" picture="box box1" />
+        <MovieTitle name="Suits" picture="box box2" />
+        <MovieTitle name="The Witcher" picture="box box3" />
 
+      </div>
 
+      <div className="container">
+
+        <MovieTitle name="Bleach" picture="box box4" />
+        <MovieTitle name="Lucifer" picture="box box5" />
+        <MovieTitle name="Fairytail" picture="box box6" />
+      </div>*/}
+
+Gallery()
     </div>
 
-    </div>
 
-   
 
   );
 
 }
 
-function MovieTitle (props) {
+function MovieTitle(props) {
+  return (
   <div className={props.picture}>
-          <div className="overlay">
-            <h2>{props.name}</h2>
-          </div>
-        </div>
+    <div className="overlay">
+      <h2>{props.name}</h2>
+    </div>
+  </div>
+  );
+}
 
+
+function Gallery () {
+  return (
+    <React.Fragment>
+    <div className="container">
+
+        <MovieTitle name="Black Mirror" picture="box box1" />
+        <MovieTitle name="Suits" picture="box box2" />
+        <MovieTitle name="The Witcher" picture="box box3" />
+
+      </div>
+
+      <div className="container">
+
+        <MovieTitle name="Bleach" picture="box box4" />
+        <MovieTitle name="Lucifer" picture="box box5" />
+        <MovieTitle name="Fairytail" picture="box box6" />
+      </div>
+      </React.Fragment>
+
+
+    
+
+
+  );
 }
 
 
