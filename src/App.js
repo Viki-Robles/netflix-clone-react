0d98d69391ef3662b83b1d1572b59913614faf7b
Vicky <vicky@arimarcssiPhone.localdomain>
2020-01-13 19:26:51 +0000

    components of the boxes

--- src/App.js
+++ src/App.js
@@ -6,57 +6,57 @@ function App() {
   return (
     <div className="App">
 
-      <body>
-        <h1>KodFlix</h1>
 
-        <nav>Nav bar</nav>
-        <div className="container">
-          <aside>Menu</aside>
-          <article>Article</article>
-        </div>
+      <h1>KodFlix</h1>
 
-        <img src={molly} className="header-image" alt='molly logo' />
-        <Gallery/>
-
-        <footer>Footer</footer>
-      </body>
-    </div>
-
-
-  );
-
-}
-
-function Movietitle(props) {
-  return (
-    <div className={props.picture}>
-      <div className="overlay">
-        <h2>{props.name}</h2>
+      <nav>Nav bar</nav>
+      <div className="container">
+        <aside>Menu</aside>
+        <article>Article</article>
       </div>
+
+      <img src={molly} className="header-image" alt='molly logo' />
+
+      <div className="container">
+
+      <MovieTitle name="Black Mirror" picture="box box1"/>
+      <MovieTitle name="Suits" picture="box box2"/>
+      <MovieTitle name="The Witcher" picture="box box3"/>
+
+      <div className="container">
+
+      <MovieTitle name="Bleach" picture="box box4"/>
+      <MovieTitle name="Lucifer" picture="box box5"/>
+      <MovieTitle name="Fairytail" picture="box box6"/>
+      </div>
+
+
+      
+
+     
+
+
+
+
     </div>
 
+    </div>
 
-
-  );
-}
-
-function Gallery(props) {
-  return (
-    <div className="container">
-          <Movietitle name="Black Mirror" picture="box box1"/>
-          <Movietitle name="Suits" picture="box box2" />
-          <Movietitle name="The Witcher" picture="box box3" />
-        </div>
-
-        <div className="container">
-          <Movietitle name="Bleach" picture="box box4" />
-          <Movietitle name="Lucifer" picture="box box5" />
-          <Movietitle name="Fairytail" picture="box box6" />
-        </div>
+   
 
   );
-  
+
 }
+
+function MovieTitle (props) {
+  <div className={props.picture}>
+          <div className="overlay">
+            <h2>{props.name}</h2>
+          </div>
+        </div>
+
+}
+
 
 
 
