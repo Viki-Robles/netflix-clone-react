1be293290b2857c177295bb7211f99a9f99f6f4d
Vicky <vicky@Vickys-MacBook-Pro.local>
2020-01-14 23:18:27 +0000

    URL ROUTING STEP 1

--- src/App.js
+++ src/App.js
@@ -1,29 +1,41 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import Gallery from './Gallery';
 
-import molly from '/MollyLogo.png';
+import molly from './MollyLogo.jpg';
 
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      <img src={molly} alt='molly logo'/>
+      <Router>
 
-      <Gallery/>
+        <div className="App">
+        <h1>KodFlix</h1>
+        <img src={molly} alt='molly logo' />
+          <Route exact path='/' component={Gallery}/>
+          <Route path='/details' component={Details}/>
 
-      </div>
+         
+
+        </div>
+      </Router>
     );
   }
 
 }
 
+function Details() {
+  return (
+   <div><h1>Hello, this will be the details page for each Movie & TV show :)</h1></div>
+  );
+}
 
 export default App;
 
 
-      
+
 
 
 
