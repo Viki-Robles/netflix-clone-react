325082772422f775a94d78a2c303f2b3608e4472
Vicky <vicky@Vickys-MacBook-Pro.local>
2020-01-14 21:30:38 +0000

    gallery component

--- src/App.js
+++ src/App.js
@@ -1,32 +1,28 @@
 import React, { Component } from 'react';
 import './App.css';
-import molly from './MollyLogo.png';
-import { BrowserRouter as Router } from 'react-router-dom';
-import MovieTitle from './MovieTitle';
 import Gallery from './Gallery';
+import MovieTitle from './MovieTitle';
+
 
 class App extends Component {
   render() {
-
     return (
       <div className="App">
-        <img src={molly} alt='molly logo' />
 
-        
+      <Gallery/>
 
       </div>
     );
-
   }
+
 }
+
 
 export default App;
 
 
-/*
-        <Gallery></Gallery>
+      
 
-*/
 
 
 
