62976e02770406a56a5dcd136bab66df100697e2
Vicky <vicky@Vickys-MacBook-Pro.local>
2020-01-14 20:47:14 +0000

    background image fix

--- src/App.js
+++ src/App.js
@@ -1,85 +1,35 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './App.css';
-import molly from './mollysgame.jpg';
-import { BrowserRouter as Router} from 'react-router-dom';
+import molly from './MollyLogo.png';
+import { BrowserRouter as Router } from 'react-router-dom';
 import MovieTitle from './MovieTitle';
 import Gallery from './Gallery';
 
 class App extends Component {
-render() {
-  
-  return (
-    <div className="App">
+  render() {
 
+    return (
+      <div className="App">
+        <img src={molly} alt='molly logo' />
 
-      <h1>KodFlix</h1>
-
-      <nav>Nav bar</nav>
-      <div className="container">
-        <aside>Menu</aside>
-        <article>Article</article>
-      </div>
-
-      <img src={molly} className="header-image" alt='molly logo' />
-
-
-
-
-      {/* <div className="container">
-
-        <MovieTitle name="Black Mirror" picture="box box1" />
-        <MovieTitle name="Suits" picture="box box2" />
-        <MovieTitle name="The Witcher" picture="box box3" />
+        
 
       </div>
+    );
 
-      <div className="container">
-
-        <MovieTitle name="Bleach" picture="box box4" />
-        <MovieTitle name="Lucifer" picture="box box5" />
-        <MovieTitle name="Fairytail" picture="box box6" />
-      </div>*/}
-
-      <Gallery>
-       
-      </Gallery>
-
-    </div>
-
-
-
-  );
-
-}
+  }
 }
 
 export default App;
 
 
-
-
-
 /*
-function Details (){
-  return (
-
-<Gallery>
-
-<a href = "http://localhost:3000/details" title="Hello, this will be the 
-details page for each Movie & TV show :)"/>
-
-/>
-
-
-
-
+        <Gallery></Gallery>
 
 */
 
 
 
-
- 
 
 
 
