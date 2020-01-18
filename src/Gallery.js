eb18bdbbb997da4f6ba536b08a3cd16d920a1d64
Vicky <vicky@Vickys-MacBook-Pro.local>
2020-01-18 23:37:57 +0000

    images changes

--- src/Gallery.js
+++ src/Gallery.js
@@ -8,13 +8,14 @@ export default function Gallery() {
       <MovieTitle id="blackMirror" name="Black Mirror" picture="box box1" />
         <MovieTitle id="suits" name="Suits" picture="box box2" />
         <MovieTitle id="theWitcher" name="The Witcher" picture="box box3" />
-        <div className="container">
+
+        
 
         <MovieTitle id="bleach" name="Bleach" picture="box box4" />
         <MovieTitle id="lacasadepapel" name="La Casa De Papel" picture="box box5" />
         <MovieTitle id="fairytail" name="Fairytail" picture="box box6" />
 
-        </div>
+     
 
       </div>
     </div>
