7286ca8f3f0662c25c83fbe1903fcf5e6e91faec
Vicky <vicky@Vickys-MacBook-Pro.local>
2020-01-19 16:03:01 +0000

    Mapping and Routing

--- src/Gallery.js
+++ src/Gallery.js
@@ -1,37 +1,28 @@
 import React from 'react';
 import MovieTitle from './MovieTitle';
+import { render } from '@testing-library/react';
+import getMovieTitle from './gallery-get';
 
 export default function Gallery() {
   return (
     <div>
       <div className="container">
-      <MovieTitle id="blackMirror" name="Black Mirror" picture="box box1" />
-        <MovieTitle id="suits" name="Suits" picture="box box2" />
-        <MovieTitle id="theWitcher" name="The Witcher" picture="box box3" />
-        <MovieTitle id="bleach" name="Bleach" picture="box box4" />
-        <MovieTitle id="lacasadepapel" name="La Casa De Papel" picture="box box5" />
-        <MovieTitle id="fairytail" name="Fairytail" picture="box box6" />
-
-
-
+        {
+          getMovieTitle().map(MovieTitle => (
+            <MovieTitle key={MovieTitle.id}
+              id={MovieTitle.id}
+              name={MovieTitle.name}
+              picture={MovieTitle.picture} />
+          ))
+        }
+</div>
       </div>
-    </div>
+   
   );
 }
 
-/*function getMovieTitle() {
-  return [
-    {id: "blackMirror", name:"Black Mirror", picture:"box box1"},
-    {id: "suits", name:"Suits", picture:"box box2"},
-    {id: "theWitcher", name:"The Witcher", picture:"box box3"},
-    {id: "bleach", name:"Bleach", picture:"box box4"},
-    {id: "lacasadepapel", name:"La Casa De Papel", picture:"box box5"},
-    {id: "fairytail", name:"Fairytail", picture:"box box6"},
 
 
-  ];
-    
-}
 
 /*
 <MovieTitle id="blackMirror" name="Black Mirror" picture="box box1" />
--- src/MovieTitle.js
+++ src/MovieTitle.js
@@ -3,15 +3,15 @@ import { Link } from 'react-router-dom';
 
 export default function MovieTitle(props) {
   return (
-<>
-<Link to ={'/' + props.id} className="box">
-    <div className={props.picture}>
-      <div className="overlay">
-        <h2>{props.name}</h2>
-      </div>
-    </div>
-  </Link>
-</>
+    <>
+      <Link to={'/' + props.id} className="box">
+        <div className={props.picture}>
+          <div className="overlay">
+            <h2>{props.name}</h2>
+          </div>
+        </div>
+      </Link>
+    </>
   );
 }
 
