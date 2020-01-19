d430edf52a03afb779391c73bd53737dbc6e03c5
Vicky <vicky@Vickys-MacBook-Pro.local>
2020-01-19 19:08:52 +0000

    error in the Gallery

--- src/Gallery.js
+++ src/Gallery.js
@@ -1,28 +1,27 @@
 import React from 'react';
 import MovieTitle from './MovieTitle';
-import { render } from '@testing-library/react';
 import getMovieTitle from './gallery-get';
+
 
 export default function Gallery() {
   return (
     <div>
       <div className="container">
+        
         {
-          getMovieTitle().map(MovieTitle => (
+          getMovietitle().map(MovieTitle => (
             <MovieTitle key={MovieTitle.id}
               id={MovieTitle.id}
               name={MovieTitle.name}
               picture={MovieTitle.picture} />
           ))
         }
-</div>
       </div>
-   
+    
+    </div>
+
   );
 }
-
-
-
 
 /*
 <MovieTitle id="blackMirror" name="Black Mirror" picture="box box1" />
@@ -31,6 +30,8 @@ export default function Gallery() {
         <MovieTitle id="bleach" name="Bleach" picture="box box4" />
         <MovieTitle id="lacasadepapel" name="La Casa De Papel" picture="box box5" />
         <MovieTitle id="fairytail" name="Fairytail" picture="box box6" />
+
+        import { render } from '@testing-library/react';
 
 */
 
--- src/MovieTitle.js
+++ src/MovieTitle.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+
 export default function MovieTitle(props) {
   return (
     <>
-      <Link to={'/' + props.id} className="box">
+      <Link to={`/ ${props.id} `} className="box">
         <div className={props.picture}>
           <div className="overlay">
             <h2>{props.name}</h2>
