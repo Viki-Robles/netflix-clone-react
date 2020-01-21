1f2e770eeffb0413556d1d200c363a59dd46c471
Vicky <vicky@Vickys-MacBook-Pro.local>
2020-01-21 22:41:13 +0000

    Iteration of the Array

--- src/Gallery.js
+++ src/Gallery.js
@@ -7,16 +7,16 @@ export default function Gallery() {
   return (
     <div>
       <div className="container">
+        {
 
 
-        <MovieTitle id="blackMirror"name="Black Mirror" picture="box box1" />
-        <MovieTitle id="suits"name="Suits" picture="box box2" />
-        <MovieTitle id="theWitcher"name="The Witcher" picture="box box3" />
-        <MovieTitle id="bleach"name="Bleach" picture="box box4" />
-        <MovieTitle id="la-casa-de-papel"name="La Casa De Papel" picture="box box5" />
-        <MovieTitle id="fairytail"name="Fairytail" picture="box box6" />
-
-
+          getMovieTitle().map(movieTitle => (
+            <MovieTitle key={movieTitle.id}
+              id={movieTitle.id}
+              name={movieTitle.name}
+              picture={movieTitle.picture} />
+          ))
+        }
       </div>
 
     </div>
@@ -25,21 +25,11 @@ export default function Gallery() {
 }
 
 /*
+
         <MovieTitle id="blackMirror" name="Black Mirror" picture="box box1" />
         <MovieTitle id="suits" name="Suits" picture="box box2" />
         <MovieTitle id="theWitcher" name="The Witcher" picture="box box3" />
         <MovieTitle id="bleach" name="Bleach" picture="box box4" />
         <MovieTitle id="lacasadepapel" name="La Casa De Papel" picture="box box5" />
         <MovieTitle id="fairytail" name="Fairytail" picture="box box6" />
-
-      
-
-
-getMovieTitle().map(movieTitle => (
-            <MovieTitle key={movieTitle.id}
-              id={movieTitle.id}
-              name={movieTitle.name}
-              picture={movieTitle.picture} />
-          ))
 */
-
--- src/gallery-get.js
+++ src/gallery-get.js
@@ -1,16 +1,17 @@
 export default function getMovieTitle() {
 
     return [
-       
+
         { id: "blackMirror", name: "Black Mirror", picture: "box box1" },
         { id: "suits", name: "Suits", picture: "box box2" },
         { id: "theWitcher", name: "The Witcher", picture: "box box3" },
         { id: "bleach", name: "Bleach", picture: "box box4" },
         { id: "lacasadepapel", name: "La Casa De Papel", picture: "box box5" },
         { id: "fairytail", name: "Fairytail", picture: "box box6" },
-       
+
 
     ];
 
 }
+
 
