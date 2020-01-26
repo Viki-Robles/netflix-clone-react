0a193a69f5b8b8e4f53e80bc71de157426bdfc80
Vicky <vicky@Vickys-MacBook-Pro.local>
2020-01-26 16:21:09 +0000

    class component

--- src/Details.js
+++ src/Details.js
@@ -2,78 +2,48 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import getMovieTitle from './gallery-get';
 
-export default function Details() {
-  return (
-    <>
-    <h1>Welcome to Vicky's app!</h1>
-    <Link to='/'><h1>Home</h1></Link>
-   </>
-  );
+
+
+export default class Details extends React.Component {
+
+      constructor() {
+        super();
+        this.state ={
+            welcomeMessage: 'Welcome to Vickys app!'
+        };
+      }
+
+      componentDidMount() {
+        setTimeout (()=> {
+
+        }, 3000);
+      }
+
+      render() {
+        return (
+          <>
+          <h1>{this.state.welcomeMessage}</h1>
+          <Link to='/'><h2>Home</h2></Link>
+         </>
+        );
+      
+      }
 }
+
+
 
 /*
 
-import { Link, Redirect } from 'react-router-dom';
-
-
-export default class Details extends Component {
-
-  constructor() {
-    super();
-    this.state = {
-      movietitle: {}
-    };
-  }
-
-
-  componentDidMount() {
-
-    let movietitleid = this.props.match.params.movietitleid;
-    let movietitle = getMovieTitle().find((movietitle) => {
-        return movietitle.id === movietitleid;
-    });
-    
-      this.setState({
-       movietitle: movietitle
-
-      });
-   
-  }
-
-  render() {
-    if(this.state.movietitle === undefined) {
-        return <Redirect to='/not-found'></Redirect>
-    } else {
-
-    return (
-      <>
-        <h1>{this.state.movietitle.name}</h1>
-        <Link to='/'><h2>Home</h2></Link>
-      </>
-    );
-  }
-}
-
-
 
 
 export default function Details() {
   return (
     <>
     <h1>Welcome to Vicky's app!</h1>
-    <Link to='/'><h1>Home</h1></Link>
+    <Link to='/'><h2>Home</h2></Link>
    </>
   );
 }
-
-import React from 'react';
-
-export default function Details() {
-  return (
-    <div><h2>Welcome to my app</h2></div>
-  );
-}
-     
 
 */
 
