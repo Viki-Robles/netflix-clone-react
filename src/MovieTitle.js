81c91274b83aa2153dc0880a641c0e506b0bc9bf
Vicky <vicky@Vickys-MacBook-Pro.local>
2020-02-01 12:02:47 +0000

    NA

--- src/MovieTitle.js
+++ src/MovieTitle.js
@@ -7,7 +7,7 @@ export default function MovieTitle(props) {
   return (
     <>
     <Link to={`/${props.id}`} className="container">
-        <img src={props.logo} alt={`${props.name} logo`}/>
+        <img src={props.logo} alt={`${props.name} logo`} className="box"/>
           <div className="overlay">
             <h2>{props.name}</h2>
             </div>
