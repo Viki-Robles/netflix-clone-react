d0305837efe7551e338ed7c1f956a6fd3e6163d2
Vicky <vicky@Vickys-MacBook-Pro.local>
2020-02-01 12:01:01 +0000

    img fixed

--- src/MovieTitle.js
+++ src/MovieTitle.js
@@ -6,11 +6,13 @@ import { Link } from 'react-router-dom';
 export default function MovieTitle(props) {
   return (
     <>
-    <Link to={`/${props.id}`} className="box">
+    <Link to={`/${props.id}`} className="container">
         <img src={props.logo} alt={`${props.name} logo`}/>
           <div className="overlay">
             <h2>{props.name}</h2>
             </div>
+          
+            
         </Link>
     </>
   );
--- src/gallery-get.js
+++ src/gallery-get.js
@@ -3,7 +3,7 @@ import Movietitle from './MovieTitle';
 import BlackMirror from './images/BlackMirror.jpg';
 import bleach from './images/bleach.jpg';
 import casadepapel from './images/casadepapel.jpg';
-import FairyTail from './images/Fairytail.jpg';
+import FairyTail from './images/FairyTail.jpg';
 import Suits from './images/Suits.jpg';
 import TheWitcher from './images/TheWitcher.jpg';
 
@@ -13,11 +13,11 @@ export default function getMovieTitle() {
 
     return [
 
-        { id: "blackMirror", name: "BlackMirror", logo: BlackMirror},
-        { id: "bleach", name: "bleach", logo: bleach },
+        { id: "blackMirror", name: "Black Mirror", logo: BlackMirror},
+        { id: "bleach", name: "Bleach", logo: bleach },
         { id: "suits", name: "Suits", logo: Suits },
-        { id: "theWitcher", name: "TheWitcher", logo: TheWitcher},
-        { id: "lacasadepapel", name: "casadepapel", logo: casadepapel},
+        { id: "theWitcher", name: "The Witcher", logo: TheWitcher},
+        { id: "lacasadepapel", name: "La Casa De Papel", logo: casadepapel},
         { id: "fairytail", name: "FairyTail", logo: FairyTail }
 
 
