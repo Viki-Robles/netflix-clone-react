5f675eec676e18d4425820b313a43fc145c52c4c
Vicky <vicky@Vickys-MacBook-Pro.local>
2020-02-01 11:48:48 +0000

    img

--- src/gallery-get.js
+++ src/gallery-get.js
@@ -3,9 +3,9 @@ import Movietitle from './MovieTitle';
 import BlackMirror from './images/BlackMirror.jpg';
 import bleach from './images/bleach.jpg';
 import casadepapel from './images/casadepapel.jpg';
-import Fairytail from './images/Fairytail.jpg';
+import FairyTail from './images/Fairytail.jpg';
 import Suits from './images/Suits.jpg';
-import TheWitcher from './images/TheWitcher';
+import TheWitcher from './images/TheWitcher.jpg';
 
 
 
@@ -18,7 +18,7 @@ export default function getMovieTitle() {
         { id: "suits", name: "Suits", logo: Suits },
         { id: "theWitcher", name: "TheWitcher", logo: TheWitcher},
         { id: "lacasadepapel", name: "casadepapel", logo: casadepapel},
-        { id: "fairytail", name: "Fairytail", logo: Fairytail },
+        { id: "fairytail", name: "FairyTail", logo: Fairytail },
 
 
 
