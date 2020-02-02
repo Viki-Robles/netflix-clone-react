a2e5be2d4c86c63b9d5e96328b6817fb77ffa657
Vicky <vicky@Vickys-MacBook-Pro.local>
2020-02-02 19:34:19 +0000

    display button

--- src/Menu/Menu.js
+++ src/Menu/Menu.js
@@ -12,21 +12,21 @@ export default class Menu extends React.Component {
 
     render() {
         return (
-            <di className="Menu">
-                <MenuButton/>
+            <div>className="Menu">
+                <MenuButton />
                 <div className="Menu-links">
                     <div className="Menu-link"><b>Home</b></div>
                     <div className="Menu-link"><b>Series</b></div>
                     <div className="Menu-link"><b>About</b></div>
                     <div className="Menu-link"><b>Contact</b></div>
                 </div>
-            </di>
+            </div>
         );
     }
 }
 
 function MenuButton() {
     return (
-        <button className="Menubutton"><b>Menu Button</b></button>
+        <button className="Menubutton"><b>MenuButton</b></button>
     );
 }
