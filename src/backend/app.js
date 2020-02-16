09e7fdda9827a6679b797dc2f2a1c924b99e9415
Vicky <vicky@Vickys-MacBook-Pro.local>
2020-02-16 11:48:21 +0000

    changes at the uI mobile

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3001
+const movies = require('./moviesback')
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('./moviesback', (req, res) => req.send(moviesback))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log({movies}))
